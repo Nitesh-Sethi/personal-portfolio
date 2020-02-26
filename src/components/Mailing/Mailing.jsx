5a61563a3684f53b1c65721cc3e5bda4ba5d12c0
Nitesh Sethi <nonusethi1272@gmail.com>
2020-02-26 13:18:16 +0530

    recommitting after reset

--- src/components/Mailing/Mailing.jsx
+++ src/components/Mailing/Mailing.jsx
@@ -6,7 +6,7 @@ function Mailing() {
     <React.Fragment>
       <div className="email-area">
         <h1>dxtrn.umf.2017@gmail.com</h1>
-        <button href="mailto:dxtrn.umf.2017@gmail.com">Send Mail</button>
+        <a href="mailto:dxtrn.umf.2017@gmail.com">Send Mail</a>
       </div>
     </React.Fragment>
   );
